90addd1bac24256504e470a3a6542087dab09d9e
Dave Lim <davelim77@gmail.com>
2024-05-13 16:25:09 -0500

    Completed chapter 2 studio

---
+++ circle/Circle.cs
@@ -0,0 +1,14 @@
+namespace Helpers {
+    public class Circle {
+        public static double Area(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        public static double Circumference(double radius) {
+            return 2 * Math.PI * radius;
+        }
+        public static double Diameter(double radius) {
+            return 2 * radius;
+        }
+    }
+}
---
+++ circle/Program.cs
@@ -0,0 +1,34 @@
+﻿using Helpers;
+
+// Area, circumference, diameter
+Console.WriteLine("Enter a radius: ");
+string? input;
+double radius;
+while (true) {
+    input = Console.ReadLine();
+    if (!double.TryParse(input, out radius)) {
+        Console.WriteLine("Sorry, please try again with a number.");
+        //return;
+        Environment.Exit(0);
+    }
+    if (radius > 0) {
+        break;
+    } else {
+        Console.WriteLine("Please enter a positive number.");
+    }
+}
+
+double area = Circle.Area(radius);
+double circumference = Circle.Circumference(radius);
+double diameter = Circle.Diameter(radius);
+Console.WriteLine("For a circle of radius " + radius + "...");
+Console.WriteLine("- area: " + area);
+Console.WriteLine("- circumference: " + circumference);
+Console.WriteLine("- diameter: " + diameter);
+
+// Road Trip!
+Console.WriteLine("Enter MPH: ");
+input = Console.ReadLine();
+double MPG = double.Parse(input);
+double numberOfGallons = circumference / MPG;
+Console.WriteLine("Gallons needed to go around circie: " + numberOfGallons);
